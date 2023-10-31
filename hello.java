db82aa1012d0304f7d7ad1341c5688ffd23238e1
ryzwan1980 <113553029+ryzwan1980@users.noreply.github.com>
2023-10-31 17:43:17 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,10 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+pipeline {
+    agent any
+    stages {
+        stage('Hello') {
+            steps {
+                sh 'echo "Hello, World!"'
+            }
+        }
     }
 }
